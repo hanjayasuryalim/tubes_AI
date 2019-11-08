2f48b9ddabec769827cf1eda2a146dfe0a204399
yosuahanjaya <hanjayasuryalim@gmail.com>
2019-11-08 20:39:28 +0700

    completing database

--- TBCAnalyzer/src/Model/Data.java
+++ TBCAnalyzer/src/Model/Data.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Model;
 
 /**
@@ -10,5 +6,132 @@ package Model;
  * @author Hanjaya
  */
 public class Data {
+    private boolean merokok;
+    private boolean kelembabanUdara;
+    private boolean keadaanRumah;
+    private boolean diabetes;
+    private boolean hiv;
+    private boolean batuk;
+    private boolean sesakNafas;
+    private boolean nyeriDada;
+    private boolean dahak;
+    private boolean demam;
+    private boolean nafsuMakan;
+    private boolean keringatMalam;
+    private boolean turunBeratBadan;
+    private boolean TBC;
+
+    public boolean isMerokok() {
+        return merokok;
+    }
+
+    public void setMerokok(boolean merokok) {
+        this.merokok = merokok;
+    }
+
+    public boolean isKelembabanUdara() {
+        return kelembabanUdara;
+    }
+
+    public void setKelembabanUdara(boolean kelembabanUdara) {
+        this.kelembabanUdara = kelembabanUdara;
+    }
+
+    public boolean isKeadaanRumah() {
+        return keadaanRumah;
+    }
+
+    public void setKeadaanRumah(boolean keadaanRumah) {
+        this.keadaanRumah = keadaanRumah;
+    }
+
+    public boolean isDiabetes() {
+        return diabetes;
+    }
+
+    public void setDiabetes(boolean diabetes) {
+        this.diabetes = diabetes;
+    }
+
+    public boolean isHiv() {
+        return hiv;
+    }
+
+    public void setHiv(boolean hiv) {
+        this.hiv = hiv;
+    }
+
+    public boolean isBatuk() {
+        return batuk;
+    }
+
+    public void setBatuk(boolean batuk) {
+        this.batuk = batuk;
+    }
+
+    public boolean isSesakNafas() {
+        return sesakNafas;
+    }
+
+    public void setSesakNafas(boolean sesakNafas) {
+        this.sesakNafas = sesakNafas;
+    }
+
+    public boolean isNyeriDada() {
+        return nyeriDada;
+    }
+
+    public void setNyeriDada(boolean nyeriDada) {
+        this.nyeriDada = nyeriDada;
+    }
+
+    public boolean isDahak() {
+        return dahak;
+    }
+
+    public void setDahak(boolean dahak) {
+        this.dahak = dahak;
+    }
+
+    public boolean isDemam() {
+        return demam;
+    }
+
+    public void setDemam(boolean demam) {
+        this.demam = demam;
+    }
+
+    public boolean isNafsuMakan() {
+        return nafsuMakan;
+    }
+
+    public void setNafsuMakan(boolean nafsuMakan) {
+        this.nafsuMakan = nafsuMakan;
+    }
+
+    public boolean isKeringatMalam() {
+        return keringatMalam;
+    }
+
+    public void setKeringatMalam(boolean keringatMalam) {
+        this.keringatMalam = keringatMalam;
+    }
+
+    public boolean isTurunBeratBadan() {
+        return turunBeratBadan;
+    }
+
+    public void setTurunBeratBadan(boolean turunBeratBadan) {
+        this.turunBeratBadan = turunBeratBadan;
+    }
+
+    public boolean isTBC() {
+        return TBC;
+    }
+
+    public void setTBC(boolean TBC) {
+        this.TBC = TBC;
+    }
+    
     
 }
